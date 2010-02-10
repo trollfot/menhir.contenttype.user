f1ac37dec7aeea91fdf7d25fb7dfff96c91a9839
Souheil CHELFOUH <trollfot@vittra.org>
2010-02-10 15:19:48 +0100

    Exposing UserView

--- src/menhir/contenttype/user/__init__.py
+++ src/menhir/contenttype/user/__init__.py
@@ -4,5 +4,6 @@ del MessageFactory
 
 from menhir.contenttype.user.user import IUser, User
 from menhir.contenttype.user.directory import Directory, IDirectory
+from menhir.contenttype.user.browser.user import UserView
 from menhir.contenttype.user.browser.forms import (
     UserAdd, UserEdit, UserPassword)
