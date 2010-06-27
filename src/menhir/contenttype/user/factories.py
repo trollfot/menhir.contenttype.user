056c8d1029914ed4b8cbc925b6ce0c019725069d
Vincent Fretin <vincent.fretin@gmail.com>
2010-06-27 20:24:55 +0200

    LocatablePrincipal inherits from Principal to avoid to copy the implementation of allGroups. Don't set groups here, it is done by dolmen.app.authentication.handlers

--- src/menhir/contenttype/user/factories.py
+++ src/menhir/contenttype/user/factories.py
@@ -8,43 +8,23 @@ from zope.event import notify
 from zope.location.interfaces import ILocation
 from zope.pluggableauth.interfaces import (
     AuthenticatedPrincipalCreated, IAuthenticatedPrincipalFactory)
+from zope.pluggableauth.factories import Principal
 from zope.publisher.interfaces import IRequest
-from zope.security.interfaces import IGroupClosureAwarePrincipal as IPrincipal
 
 
-class LocatablePrincipal(object):
-    grok.implements(IPrincipal, ILocation)
+class LocatablePrincipal(Principal):
+    grok.implements(ILocation)
 
     def __init__(self, info):
         self.id = info.id
         self.title = info.title
         self.description = info.description
-        self.groups = ["zope.Authenticated", "zope.Everybody"]
+        self.groups = []
         self.__name__ = info.__name__
         self.__parent__ = info.__parent__
 
     def __repr__(self):
         return 'LocatablePrincipal(%r)' % self.id
-
-    @property
-    def allGroups(self):
-        """This needs to be computed somehow.
-        """
-        if self.groups:
-            seen = set()
-            principals = getUtility(IAuthentication)
-            stack = [iter(self.groups)]
-            while stack:
-                try:
-                    group_id = stack[-1].next()
-                except StopIteration:
-                    stack.pop()
-                else:
-                    if group_id not in seen:
-                        yield group_id
-                        seen.add(group_id)
-                        group = principals.getPrincipal(group_id)
-                        stack.append(iter(group.groups))
 
 
 class AuthenticatedPrincipalFactory(grok.MultiAdapter):
